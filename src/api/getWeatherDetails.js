47f00f7e29308c0f867aeac807c0ecdf2b0044d0
Rajvee <rajveedkadchha@gmail.com>
2024-07-24 17:27:10 +0530

    api changed

--- src/api/getWeatherDetails.js
+++ src/api/getWeatherDetails.js
@@ -1,16 +1,30 @@
 import axios from "axios";
 const API_KEY = "94c685d967b1435e809178941442ca0f";
 
+// export const getWeatherData = (city, today_date, end_date) => {
+//   return axios.get("https://api.weatherbit.io/v2.0/history/energy", {
+//     params: {
+//       city: city,
+//       start_date: today_date,
+//       end_date: end_date,
+//       threshold: 63,
+//       units: "I",
+//       key: API_KEY,
+//       tp: "daily",
+//     },
+//   });
+// };
+
 export const getWeatherData = (city, today_date, end_date) => {
-  return axios.get("https://api.weatherbit.io/v2.0/history/energy", {
+  return axios.get("https://api.weatherbit.io/v2.0/current", {
     params: {
       city: city,
-      start_date: today_date,
-      end_date: end_date,
-      threshold: 63,
+      // start_date: today_date,
+      // end_date: end_date,
+      // threshold: 63,
       units: "I",
       key: API_KEY,
-      tp: "daily",
+      include: "minutely",
     },
   });
 };
